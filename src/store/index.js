f0a67383e36db9b28a288f6cda28e1e7787fd8b8
lee <3024169080@qq.com>
2019-01-16 10:02:34 +0800

    develop修改store/index

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+Vue.use(Vuex)
 export default new Vuex.Store({
   state:{
 
@@ -8,6 +9,6 @@ export default new Vuex.Store({
 
   },
   mutations:{
-    
+
   }
 })
